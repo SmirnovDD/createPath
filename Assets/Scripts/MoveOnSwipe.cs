c00af0787a406401898632cf7bb455b91f4e3640
SmirnovDD <studioindevelopment@gmail.com>
2018-10-19 11:28:55 +0300

    Initial Commit

---
+++ Assets/Scripts/MoveOnSwipe.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnSwipe : MonoBehaviour {
+    [HideInInspector]
+    public Vector3 startPos;
+
+    private void OnEnable()
+    {
+         RegisterSwipe.OnSwipe += Move;
+    }
+    private void OnDisable()
+    {
+        RegisterSwipe.OnSwipe -= Move;
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        startPos = transform.position; // в будущем убрать
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Move(RegisterSwipe.Swipes swipeDir)
+    {
+        if (swipeDir == RegisterSwipe.Swipes.right)
+        {
+            transform.position = transform.position + Vector3.right;
+        }
+        else if (swipeDir == RegisterSwipe.Swipes.left)
+        {
+            transform.position = transform.position + Vector3.left;
+        }
+    }
+}
---
+++ Assets/Scripts/RegisterSwipe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterSwipe : MonoBehaviour {
+    public enum Swipes
+    {
+        right,
+        left
+    };
+
+    public delegate void Swipe(Swipes swipeDir);
+    public static event Swipe OnSwipe;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.D))
+        {
+            if (OnSwipe != null)
+                OnSwipe(Swipes.right);
+        }
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            if (OnSwipe != null)
+                OnSwipe(Swipes.left);
+        }
+    }
+}
